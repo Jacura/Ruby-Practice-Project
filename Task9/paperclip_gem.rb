12e496f2442b438d081f2e5761722c8d22de341f
Ankit Rawal <18305@iiitu.ac.in>
2021-10-21 10:13:56 +0530

    add paperclip gem and reserved keywords

---
+++ Task9/paperclip_gem.rb
@@ -0,0 +1,11 @@
+=begin
+
+Paperclip is intended as an easy file attachment library for ActiveRecord.
+The intent behind it was to keep setup as easy as possible and to treat files
+as much like other attributes as possible.
+
+SETTING UP:
+For PaperClip to work, we need to have ImageMagick installed on our machine.
+
+Ubuntu users should run the command:
+sudo apt-get install imagemagick -y
---
+++ Task9/reserved_keyword.rb
@@ -0,0 +1,21 @@
+#Reserved Words/Keywords in Ruby
+
+=begin
+
+There are certain words that are reserved for doing specific tasks. 
+These words are known as keywords and they have standard, predefined meaning in Ruby.
+
+Some of Reserved Keywords:
+BEGIN               Code, enclosed in { }, to run before the program runs.
+END                 Code, enclosed in { }, to run when the program ends.
+alias               Creates an alias for an existing method, operator, or global variable.
+and                 Logical operator; same as && except and has lower precedence.
+begin               Begins a code block or group of statements; closes with end.
+break               Terminates a while or until loop, or a method inside a block.
+case                Compares an expression with a matching when clause; closes with end. See when.
+class               Defines a class; closes with end.
+def                 Defines a method; closes with end.
+
+
+=end
+
