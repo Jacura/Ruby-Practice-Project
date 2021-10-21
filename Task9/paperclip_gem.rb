c0b03148d34cba817c1830629f38c67b260325be
Ankit Rawal <18305@iiitu.ac.in>
2021-10-21 10:17:47 +0530

    update paperclip part

--- Task9/paperclip_gem.rb
+++ Task9/paperclip_gem.rb
@@ -6,6 +6,13 @@ as much like other attributes as possible.
 
 SETTING UP:
 For PaperClip to work, we need to have ImageMagick installed on our machine.
-
 Ubuntu users should run the command:
 sudo apt-get install imagemagick -y
+
+OBJECTIVE
+1> Use the Paperclip gem to upload photos as attachments to models
+2> Use a default image when nothing has been attached .
+3> Understand image processing and resizing with Paperclip.
+
+
+=end
