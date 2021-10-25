c104ff7d5637a710e7db0cc6fd37e8aa91156370
Ankit Rawal <18305@iiitu.ac.in>
2021-10-25 09:43:07 +0530

    add more tasks

---
+++ Task10/active_admin_gem.rb
@@ -0,0 +1,16 @@
+=begin 
+Active Admin is a framework for creating administration style interfaces.
+It abstracts common business application patterns to make it simple for 
+developers to implement beautiful and elegant interfaces with very little effort.
+
+# Gemfile
+ gem 'activeadmin'
+
+# After updating your bundle, run the installer
+ rails generate active_admin:install
+
+# Migrate your db and start the server
+ rails db:migrate
+ rails server
+
+=end
---
+++ Task10/dynamic_static_typing.rb
@@ -0,0 +1,25 @@
+=begin
+
+Type Checking :
+
+Type checking is a program analysis that verifies something about the types that are used in the program.
+The purpose of type checking is to guarantee type safety.
+Type checking could happen at compile-time or at run-time.
+There is two types of type checking:
+1> Static Type Typing
+2> Dynamic Type Typing
+
+1) Static type checking: When variable data type is defined.
+For example, in Java,C ,C++
+int age = 20;
+
+It is performed at compile time.
+2) Dynamic type checking: When variable data type is not defined. Dynamic type checking
+ results into less optimized code and runtime type error which is likely to occur 
+ since it forces runtime checks every time the program executes.
+For example, in Python , Ruby ,
+age = 20;
+
+It is performed at run time.
+
+=end
